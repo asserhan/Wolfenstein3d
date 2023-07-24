eb685d60822394c8dc584002263b565db152840c
Hanane asseraou <asseraouhanane@gmail.com>
2023-07-24 17:16:30 +0100

    [fix]parse array2D

--- src/parsing/check_map.c
+++ src/parsing/check_map.c
@@ -6,7 +6,7 @@
 /*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 11:23:15 by hasserao          #+#    #+#             */
-/*   Updated: 2023/07/22 22:26:53 by hasserao         ###   ########.fr       */
+/*   Updated: 2023/07/24 16:24:22 by hasserao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,33 +57,103 @@ void rows_cols_num(t_map *map)
 	}
 		
 }
-int is_wall(char c)
+
+int ft_is_wall(char c)
 {
-	return(c == '1');
+	if(c == '1')
+		return(1);
+	return(0);
+}
+int ft_is_space(char c)
+{
+	if(c == ' ' || c == '\t' || c == '\v' || c == '\f')
+		return(1);
+	return(0);
 }
 char *skip_spaces(char *line)
 {
-	while(*line == ' ')
+
+	while(ft_is_space(*line))
 		line++;
 	return(line);
 }
+char *trim_spaces(char *line)
+{
+	int i;
+	i = ft_strlen(line) - 1;
+	while(ft_is_space(line[i]))
+		i--;
+	line[i + 1] = '\0';
+	return(line);	
+}
+char *d_space(char *line)
+{
+	char *str;
+	str = skip_spaces(line);
+	str = trim_spaces(str);
+	return(str);
+}
+
+
 int check_borders(t_map *map)
 {
 	int i;
-	char c;
+	int j;
 
+	 rows_cols_num(map);
 	i = 0;
-	rows_cols_num(map);
-	while(i < map->cols)
+	while(map->map[i] && i >= 0 && i < map->rows)
 	{
-		c = *skip_spaces(map->map[0]);
-		if(!is_wall(c + i))
+		d_space(map->map[i]) ;
+		j = 0;
+		while(map->map[i][j] && j >= 0 && j < map->cols)
 		{
-			printf("%c\n",c + i);	
-			return(1);
+	
+			if(ft_is_space(map->map[i][j]))
+			{
+				if((i - 1) >=0 && !ft_is_space(map->map[i - 1][j]) && !ft_is_wall(map->map[i - 1][j]))
+					return(1);
+				if((i + 1) < map->rows && !ft_is_space(map->map[i + 1][j]) && !ft_is_wall(map->map[i + 1][j]))
+					return(1);
+				if((j - 1) >= 0 && !ft_is_space(map->map[i][j - 1]) && !ft_is_wall(map->map[i][j - 1]))	
+					return(1);
+				if((j + 1) < map->cols && !ft_is_space(map->map[i][j + 1]) && !ft_is_wall(map->map[i][j + 1]))	
+					return(1);
+			}
+				j++;
 		}
 		i++;
-		printf("%d\n",i);
+	}
+	i = 0;
+	while(map->map[0][i])
+	{
+		if(ft_is_wall(*d_space(map->map[0] + i)) ==0)
+			return(1);
+		i++;
+	}
+	i = 0;
+	while(map->map[map->rows - 1][i])
+	{
+		if(ft_is_wall(*d_space(map->map[map->rows - 1] + i)) ==0)
+			return(1);
+		i++;
+	}
+	i = 0;
+	while(map->map[i])
+	{
+		if(ft_is_wall(*d_space(map->map[i])) ==0)
+			return(1);
+		i++;
+	}
+	i = 0;
+	while(map->map[i])
+	{
+		j = 0;
+		while(map->map[i][j])
+			j++;
+		if(ft_is_wall(map->map[i][j - 1]) == 0)
+			return(1);
+		i++;
 	}
 	return(0);
 
