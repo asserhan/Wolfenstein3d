0b234709199040cab1c6c0ac510d7f9cfdf87401
proxy <aittaleb108@gmail.com>
2023-09-15 14:03:12 +0100

    rome sq border

--- src/draw_helpers/draw_square.c
+++ src/draw_helpers/draw_square.c
@@ -6,7 +6,7 @@
 /*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 11:53:18 by otait-ta          #+#    #+#             */
-/*   Updated: 2023/07/20 18:26:59 by otait-ta         ###   ########.fr       */
+/*   Updated: 2023/09/15 13:44:10 by otait-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ void	draw_square(t_mlx_info *mlx_info, int *pixel_cord, int square_size,
 		while (j < square_size)
 		{
 			// draw border with black
-			if (i == 0 || j == 0 || i == square_size - 1 || j == square_size
-				- 1)
-				draw_pixel(mlx_info, pixel_cord[0] + i, pixel_cord[1] + j,
-						BLUE);
-			else
+			// if (i == 0 || j == 0 || i == square_size - 1 || j == square_size
+			// 	- 1)
+			// 	draw_pixel(mlx_info, pixel_cord[0] + i, pixel_cord[1] + j,
+			// 			BLUE);
+			// else
 				draw_pixel(mlx_info, pixel_cord[0] + i, pixel_cord[1] + j,
 						color);
 			j++;
