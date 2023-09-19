f5ae626e60f7cad9424c8295af2c07510a60e957
proxy <aittaleb108@gmail.com>
2023-09-19 14:36:05 +0100

    add parinsg

---
+++ src/parsing/check_map.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 11:23:15 by hasserao          #+#    #+#             */
+/*   Updated: 2023/09/19 14:33:56 by otait-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+int check_file(char *file)
+{
+
+    if (ft_strchr(file, '.') == NULL || ft_strcmp(ft_strchr(file, '.'), ".cub"))
+        return (1);
+    return (0);
+}
+int check_char(t_map *map)
+{
+    int i;
+    int j;
+    i = -1;
+    map->player_num = 0;
+    while (map->map[++i])
+    {
+        j = -1;
+        while (map->map[i][++j])
+        {
+            if (map->map[i][j] != '1' && map->map[i][j] != '0' && map->map[i][j] != 'N' && map->map[i][j] != 'W' && map->map[i][j] != 'S' && map->map[i][j] != 'E' && map->map[i][j] != ' ')
+                return (1);
+
+            if (map->map[i][j] == 'N' || map->map[i][j] == 'S' || map->map[i][j] == 'W' || map->map[i][j] == 'E')
+                map->player_num++;
+        }
+    }
+    if (map->player_num != 1)
+        return (1);
+    return (0);
+}
+void rows_cols_num(t_map *map)
+{
+    map->rows = 0;
+    map->cols = 0;
+    while (map->map[map->rows])
+    {
+        while (map->map[map->rows][map->cols])
+            map->cols++;
+        map->rows++;
+    }
+}
+int is_wall(char *line)
+{
+
+    while (*line)
+    {
+        if (*line == '0')
+            return (1);
+        line++;
+    }
+    return (0);
+}
+char *skip_spaces(char *line)
+{
+
+    while (*line == ' ')
+        line++;
+    return (line);
+}
+int check_borders(t_map *map)
+{
+
+    int i;
+    int j;
+    i = -1;
+    // print_matrix(map->map);
+    while (++i < map->rows)
+    {
+        j = -1;
+
+        map->map[i] = ft_strtrim(map->map[i], white_spaces);
+        map->map[i + 1] = ft_strtrim(map->map[i + 1], white_spaces);
+
+        if (map->map[i][0] != '1' || map->map[i][ft_strlen(map->map[i]) - 1] != '1')
+        {
+            return (1);
+        }
+
+        while (map->map[i][++j])
+        {
+            if (!ft_strchr("10NSEW \t", map->map[i][j]))
+            {
+                return (1);
+            }
+            if (ft_strchr("NSEW", map->map[i][j]))
+            {
+                map->player_num++;
+                printf("player num %d\n", map->player_num);
+                if (map->player_vue != '\0')
+                {
+                    return (1);
+                }
+                map->player_vue = map->map[i][j];
+                printf("player vue %c\n", map->player_vue);
+                map->player_x = j;
+                map->player_y = i;
+                printf("player x %d\n", map->player_x);
+                printf("player y %d\n", map->player_y);
+            }
+        }
+    }
+
+    if (map->player_num != 1)
+    {
+        printf("here\n");
+        return (1);
+    }
+
+    return (0);
+}
---
+++ src/parsing/utils.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 14:58:11 by hasserao          #+#    #+#             */
+/*   Updated: 2023/09/19 14:34:53 by otait-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+int ft_strcmp(const char *s1, const char *s2)
+{
+    if (!s1)
+        return (-1);
+    while ((*(unsigned char *)s1 || *(unsigned char *)s2))
+    {
+        if (*(unsigned char *)s1 > *(unsigned char *)s2)
+            return (*(unsigned char *)s1 - *(unsigned char *)s2);
+        else if (*(unsigned char *)s1 < *(unsigned char *)s2)
+            return (*(unsigned char *)s1 - *(unsigned char *)s2);
+        s1++;
+        s2++;
+    }
+    return (0);
+}
+int matrix_size(char **tab)
+{
+    int count;
+    count = 0;
+    while (*tab)
+    {
+        count++;
+        tab++;
+    }
+    return (count);
+}
+void print_matrix(char **tab)
+{
+    int i;
+    int size = matrix_size(tab);
+    i = 0;
+    while (i < size)
+    {
+        ft_printf("**%s**\n", tab[i]);
+        i++;
+    }
+}
+void free_matrix(char **tab)
+{
+    int i;
+    i = -1;
+    while (++i)
+        free(tab[i]);
+    free(tab);
+}
