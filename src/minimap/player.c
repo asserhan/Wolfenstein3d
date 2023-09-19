b954d61cd8d239a59c06b6612180f0a653a08bb7
proxy <aittaleb108@gmail.com>
2023-09-19 16:21:09 +0100

    move speed

--- src/minimap/player.c
+++ src/minimap/player.c
@@ -6,7 +6,7 @@
 /*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 09:56:07 by otait-ta          #+#    #+#             */
-/*   Updated: 2023/09/19 15:21:51 by otait-ta         ###   ########.fr       */
+/*   Updated: 2023/09/19 16:17:18 by otait-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void init_player(t_player *player)
 		player->facing_right = 0;
 	else if (player->rotation_angle >= 3 * M_PI_2 && player->rotation_angle <= 2 * M_PI)
 		player->facing_right = 1;
-	player->move_speed = 5;
+	player->move_speed = 15;
 	player->rotation_speed = 0.15;
 }
 
